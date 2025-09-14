47ccabb17fbd332bc6e0c7ab6fe0774f78a53c88
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-09-14 14:27:54 +0530

    add authenticatin

--- index.js
+++ index.js
@@ -10,7 +10,38 @@ const SOCKS_VERSION = 0x05;
 
 const server = createServer((clientSocket) => {
   clientSocket.once("data", (data) => {
-    console.log(data);
+    if (data[0] !== SOCKS_VERSION) {
+      console.log("Socket version mismatch: " + clientSocket.remoteAddress);
+      clientSocket.end();
+      return;
+    }
+
+    clientSocket.write(Buffer.from([SOCKS_VERSION, 0x02]));
+
+    //Handle authentication
+    clientSocket.once("data", (authData) => {
+      const ulen = authData[1];
+      const username = authData.slice(2, 2 + ulen).toString();
+      const plen = authData[2 + ulen];
+      const password = authData.slice(3 + ulen, 3 + ulen + plen).toString();
+
+      if (username === USERNAME && password === PASSWORD) {
+        clientSocket.write(Buffer.from([0x01, 0x00]));
+        console.log("New client connected:", clientSocket.remoteAddress);
+      } else {
+        clientSocket.write(Buffer.from([0x01, 0x01]));
+        clientSocket.end();
+        console.log(
+          "Authentication error: " +
+            clientSocket.remoteAddress +
+            " Username: " +
+            username +
+            " Password: " +
+            password
+        );
+        return;
+      }
+    });
   });
 
   clientSocket.on("error", (err) => {
