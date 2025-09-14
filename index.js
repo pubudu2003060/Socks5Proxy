92e0ab01115ddee3d1496af585f384da134a5ed2
pubudu2003060 <madhushanpubudu2@gmail.com>
2025-09-14 14:26:05 +0530

    check what data come from make a curl request to proxy

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ dotenv.config();
 
 const LISTEN_PORT = process.env.PROXY_PORT;
 
-const server = createServer((clientSocket) => {});
+// Use SOCKSV5
+const SOCKS_VERSION = 0x05;
+
+const server = createServer((clientSocket) => {
+  clientSocket.once("data", (data) => {
+    console.log(data);
+  });
+
+  clientSocket.on("error", (err) => {
+    console.error("Client error:" + err.message);
+  });
+});
 
 server.listen(LISTEN_PORT, () => {
   console.log("SOCKS5 proxy server running on port: " + LISTEN_PORT);
